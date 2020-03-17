a9243056258bcfcbbe224f483021965b06eb9879
yowainwright <yowainwright@gmail.com>
2020-03-16 18:35:57 -0700

    patch: prefer functional Array.from

--- bin.js
+++ bin.js
@@ -1,23 +1,23 @@
 #!/usr/bin/env node
 
-const program = require('commander')
-  .version(require('./package.json').version)
-  .usage('[options] <files>')
-  .parse(process.argv)
+const program = require("commander")
+  .version(require("./package.json").version)
+  .usage("[options] <files>")
+  .parse(process.argv);
 
-const globby = require('globby')
-const chalk = require('chalk')
-const fs = require('fs')
+const globby = require("globby");
+const chalk = require("chalk");
+const fs = require("fs");
 
-const files = globby.sync(program.args)
+const files = globby.sync(program.args);
 
-files.forEach((file) => {
-  const str = fs.readFileSync(file, 'utf8')
+Array.from(files, file => {
+  const str = fs.readFileSync(file, "utf8");
   try {
-    JSON.parse(str)
-    console.log(chalk.green(`✓ ${file}`))
+    JSON.parse(str);
+    console.log(chalk.green(`✓ ${file}`));
   } catch (_) {
-    process.exitCode = 1
-    console.error(chalk.red(`✕ ${file}`))
+    process.exitCode = 1;
+    console.error(chalk.red(`✕ ${file}`));
   }
-})
+});
